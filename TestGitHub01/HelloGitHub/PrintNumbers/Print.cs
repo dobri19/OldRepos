576cccaa9474f847077d7038b325006379975e93
dobri19 <dobri19@users.noreply.github.com>
2017-10-14 18:10:33 +0300

    AFolder

---
+++ TestGitHub01/HelloGitHub/PrintNumbers/Print.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PrintNumbers
+{
+    public class Print
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(1);
+            Console.WriteLine(11);
+            Console.WriteLine(111);    
+        }
+    }
+}
